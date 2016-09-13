2c3bfdfd8b62ba00c64509ddb705338a281eb1ac
Angus MacIsaac <donaldamacisaac@gmail.com>
2016-09-12 22:59:54 -0400

    Setup heroku deploy

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,17 @@ use Mix.Config
 # which you typically run after static files are built.
 config :hn, Hn.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "polar-reaches-95219", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+config :hello_phoenix, HelloPhoenix.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true 
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -59,7 +68,3 @@ config :logger, level: :info
 # for the new static assets to be served after a hot upgrade:
 #
 #     config :hn, Hn.Endpoint, root: "."
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
